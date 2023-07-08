fe8bdfa48564b9d57b2c210c993fa1e295a5eebd
DESKTOP-3CAB7UL\LEE <ydu@vms-solutions.com>
2023-07-08 09:40:34 +0900

    site 설정 수정

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "VMS Solutions",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
+    role: "Virtual Manufacturing System",
+    bio: "",
+    email: "support@vms-solutions.com",
     linkedin: "morethanmin",
-    github: "morethanmin",
+    github: "vmslab",
     instagram: "",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `VMS Blog`,
+      href: "https://github.com/Dong-Eun-Lee/morethan-log",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "VMS Blog",
+    description: "welcome to VMS Blog!",
     theme: "auto", // ['light', 'dark', 'auto']
   },
 
